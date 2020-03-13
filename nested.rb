e9c538e3691ec32425d32e055990344d40b5dc27
Alejandra I. Lee Vargas <leevargasale@gmail.com>
2020-03-13 04:16:19 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -83,10 +83,10 @@ def adding_matz
      }
 
  programmer_hash[:yukihiro_matsumoto] = {}
- programmer_hash[:yukihiro_matsumoto] = {:known_for ={}, :languages =[]}
- programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
- programmer_hash[:yukihiro_matsumoto][:languages] << "LISP"
- programmer_hash[:yukihiro_matsumoto][:languages] << "C"
+# programmer_hash[:yukihiro_matsumoto] = {:known_for ={}, :languages =[]}
+# programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
+# programmer_hash[:yukihiro_matsumoto][:languages] << "LISP"
+# programmer_hash[:yukihiro_matsumoto][:languages] << "C"
     return programmer_hash
 end
 
